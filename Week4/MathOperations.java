0abb48d002ddabf3a879628d74632f9ddf6a543b
Ayush Neupane <np01ai4a250082@islingtoncollege.edu.np>
2025-11-20 13:05:14 +0545

    Week-4 work

---
+++ Week4/MathOperations.java
@@ -0,0 +1,23 @@
+package Week4;
+import java.util.Scanner;
+
+
+/**
+ * Write a description of class Workshop here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MathOperations
+{
+    public static void main(String[]arg)
+    {
+         Scanner Oper = new Scanner(System.in);
+         System.out.println("Enter your desired number: ");
+         int value = Oper.nextInt();
+         String pass = "Passed";
+         String fail = "Failed";
+         String out = (value>40)?pass:fail;
+         System.out.println(out);
+    }
+}
