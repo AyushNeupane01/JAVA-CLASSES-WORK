405c3816efd0a4c840df8b0237b37e9cf598da53
Ayush Neupane <np01ai4a250082@islingtoncollege.edu.np>
2025-12-10 20:02:03 +0545

    Update Matrix.java

--- week6/Matrix.java
+++ week6/Matrix.java
@@ -8,11 +8,19 @@ import java.util.Scanner;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Matrix
-{
-   public static void main(String[]arg)
-   {
-       Scanner sc = new Scanner(System.in);
-       
-   }
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] m = new int[2][2];
+        for(int i=0;i<2;i++)
+            for(int j=0;j<2;j++)
+                m[i][j] = sc.nextInt();
+        for(int i=0;i<2;i++) {
+            for(int j=0;j<2;j++)
+                System.out.print(m[i][j] + " ");
+            System.out.println();
+        }
+    }
 }
